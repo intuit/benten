e5a5d4c5fd574f12a47b80bcee3316ccb2d3a99c
jakeleveroni <jleveroni@lawfirmmatrix.com>
2019-10-09 23:56:55 -0700

    updated hackernews actions to fit convention

--- benten-hackernews-bolt/src/main/java/com/intuit/benten/hackernews/actionhandlers/HackernewsActions.java
+++ benten-hackernews-bolt/src/main/java/com/intuit/benten/hackernews/actionhandlers/HackernewsActions.java
@@ -4,11 +4,11 @@ package com.intuit.benten.hackernews.actionhandlers;
  * Created by jleveroni on 10/09/2019
  */
 public class HackernewsActions {
-    public static final String ACTION_HACKERNEWS_GET_TOP_STORIES = "ACTION_HACKERNEWS_GET_TOP_STORIES";
-    public static final String ACTION_HACKERNEWS_GET_NEW_STORIES = "ACTION_HACKERNEWS_GET_NEW_STORIES";
-    public static final String ACTION_HACKERNEWS_GET_BEST_STORIES = "ACTION_HACKERNEWS_GET_BEST_STORIES";
-    public static final String ACTION_HACKERNEWS_GET_LATEST_ASKS = "ACTION_HACKERNEWS_GET_LATEST_ASKS";
-    public static final String ACTION_HACKERNEWS_GET_LATEST_SHOW_STORIES = "ACTION_HACKERNEWS_GET_LATEST_SHOW_STORIES";
-    public static final String ACTION_HACKERNEWS_GET_LATEST_JOB_STORIES = "ACTION_HACKERNEWS_GET_LATEST_JOB_STORIES";
-    public static final String ACTION_HACKERNEWS_GET_NEWEST_CONTENT_ID = "ACTION_HACKERNEWS_GET_NEWEST_CONTENT_ID";
+    public static final String ACTION_HACKERNEWS_GET_TOP_STORIES = "action_hackernews_get_top_stories";
+    public static final String ACTION_HACKERNEWS_GET_NEW_STORIES = "action_hackernews_get_new_stories";
+    public static final String ACTION_HACKERNEWS_GET_BEST_STORIES = "action_hackernews_get_best_stories";
+    public static final String ACTION_HACKERNEWS_GET_LATEST_ASKS = "action_hackernews_latest_asks";
+    public static final String ACTION_HACKERNEWS_GET_LATEST_SHOW_STORIES = "action_hackernews_get_latest_show_stories";
+    public static final String ACTION_HACKERNEWS_GET_LATEST_JOB_STORIES = "action_hackernews_get_latest_job_stories";
+    public static final String ACTION_HACKERNEWS_GET_NEWEST_CONTENT_ID = "action_hackernews_newest_content_id";
 }
