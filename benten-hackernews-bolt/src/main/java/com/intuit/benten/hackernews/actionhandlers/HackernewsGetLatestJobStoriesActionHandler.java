6228296641322c1ba4f53772ed6cd19836d6886b
jleveroni <jacob_leveroni@intuit.com>
2019-10-12 04:05:52 -0700

    added action handlers for the rest of the get collection type of api queries

---
+++ benten-hackernews-bolt/src/main/java/com/intuit/benten/hackernews/actionhandlers/HackernewsGetLatestJobStoriesActionHandler.java
@@ -0,0 +1,16 @@
+package com.intuit.benten.hackernews.actionhandlers;
+
+import com.intuit.benten.common.actionhandlers.BentenActionHandler;
+import com.intuit.benten.common.annotations.ActionHandler;
+import com.intuit.benten.hackernews.utils.HackernewsConstants;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jleveroni on 10/11/2019
+ */
+@Component
+@ActionHandler(action = HackernewsConstants.HackernewsActions.ACTION_HACKERNEWS_GET_LATEST_JOB_STORIES)
+public class HackernewsGetLatestJobStoriesActionHandler extends BaseHackernewsGetCollectionAction implements BentenActionHandler {
+
+}
+
